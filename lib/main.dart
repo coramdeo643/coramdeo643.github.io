e467bcd36080f2c142191f32ea4d364f31a80717
coramdeo643 <coramdeo643@kakao.com>
2025-11-14 22:22:13 +0900

    feat: Add Provider Scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_blog/core/app_theme.dart';
 import 'package:github_blog/pages/home_page.dart';
 
@@ -8,7 +9,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
